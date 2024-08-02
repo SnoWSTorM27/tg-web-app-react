fca9849d5a31d4dbf02c251a057b3624f05e2f83
SnoWSTorM <snowstorm2701@gmail.com>
2024-08-02 15:46:26 +0700

    checked functional5

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
 
 function App() {
   const tg: WebApp = window.Telegram.WebApp;
-  console.log(tg);
+  const [text, setText] = useState("");
   
 
   useEffect(() => {
@@ -16,21 +16,33 @@ function App() {
   const showAlert = () => {
     tg.showAlert("Приём алерт", ()=>{})
   };
-  const changeExpand = () => {
-    tg.expand()
+  const requestContacts = () => {
+    tg.requestContact(()=>{
+      setText("contact");
+    })
+  };
+  const showConfirm = () => {
+    tg.showConfirm("Готов?", () => {
+      setText("ready");
+    })
   };
   const openTGLink = () => {
-    tg.openTelegramLink("https://t.me/SnoWSTorM27")
+    tg.openTelegramLink("https://t.me/hamster_kombaT_bot");
   };
+  // const openInvoice = () => {
+  //   tg.openInvoice("https://t.me/hamster_kombaT_bot");
+  // };
 
   return (
     <div className='App'>
       <button className='btn' onClick={onClose}>Закрыть</button>
       <button className='btn' onClick={showAlert}>Alert</button>
-      <button className='btn' onClick={changeExpand}>expand</button>
+      <button className='btn' onClick={showConfirm}>Open Confirm</button>
       <button className='btn' onClick={openTGLink}>Open Link</button>
+      <button className='btn' onClick={requestContacts}>Request Contact</button>
+      {/* <button className='btn' onClick={tg.openInvoice("https://t.me")}>Open Invoice</button> */}
       <div>{tg.platform}</div>
-      <div>{tg.isExpanded}</div>
+      <div className='red'>{text}</div>
     </div>
   )
 }
